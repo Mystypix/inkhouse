a23af7a962d3afae24bfd1a8a071b3ec595df333
Jakub Smagin <jakub.smagin@mews.com>
2025-04-08 19:35:16 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 import react from "@astrojs/react";
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), partytown({ config: { forward: ['dataLayer.push'] } })],
   output: 'server',
   adapter: netlify(),
 });
